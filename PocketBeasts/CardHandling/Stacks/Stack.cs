77fa07a9a8d0f1f31b57fd5f580d78df1f26f5cf
KunoDLK <kunodlk@gmail.com>
2024-03-21 15:32:26 +0000

    added missed sort on card adding

--- PocketBeasts/CardHandling/Stacks/Stack.cs
+++ PocketBeasts/CardHandling/Stacks/Stack.cs
@@ -1,65 +1,66 @@
 ﻿namespace PocketBeasts.CardHandling.Stacks
 {
-    public class Stack
-    {
-        private string _name;
-        private List<Card> cards;
+      public class Stack
+      {
+            private string _name;
+            private List<Card> cards;
 
-        public Stack(string name, List<Card> cards)
-        {
-            Name = name;
-            Cards = cards;
-        }
-
-        public Stack(string name)
-        {
-            Name = name;
-            Cards = new List<Card>();
-        }
-
-        public string Name
-        {
-            get { return _name; }
-            private set { _name = value; }
-        }
-
-        public int Count
-        {
-            get { return cards.Count; }
-        }
-
-        public List<Card> Cards
-        {
-            get { return cards; }
-            private set { cards = value; }
-        }
-
-        public Card Draw()
-        {
-            Card card = Cards[0];
-            Cards.RemoveAt(0);
-            return card;
-        }
-
-        public void Remove(Card card)
-        {
-            bool sucsesfull = cards.Remove(card);
-
-            if (!sucsesfull)
+            public Stack(string name, List<Card> cards)
             {
-                throw new Exception($"Failed to find card in the \"{Name}\" stack");
+                  Name = name;
+                  Cards = cards;
             }
-        }
 
-        public void RemoveAll(List<Card> cards)
-        {
-            this.cards.RemoveAll(cards.Contains);
-        }
+            public Stack(string name)
+            {
+                  Name = name;
+                  Cards = new List<Card>();
+            }
 
-        internal void Add(Card card)
-        {
-            Cards.Add(card);
-        }
-    }
+            public string Name
+            {
+                  get { return _name; }
+                  private set { _name = value; }
+            }
+
+            public int Count
+            {
+                  get { return cards.Count; }
+            }
+
+            public List<Card> Cards
+            {
+                  get { return cards; }
+                  private set { cards = value; }
+            }
+
+            public Card Draw()
+            {
+                  Card card = Cards[0];
+                  Cards.RemoveAt(0);
+                  return card;
+            }
+
+            public void Remove(Card card)
+            {
+                  bool sucsesfull = cards.Remove(card);
+
+                  if (!sucsesfull)
+                  {
+                        throw new Exception($"Failed to find card in the \"{Name}\" stack");
+                  }
+            }
+
+            public void RemoveAll(List<Card> cards)
+            {
+                  this.cards.RemoveAll(cards.Contains);
+            }
+
+            internal void Add(Card card)
+            {
+                  Cards.Add(card);
+                  Cards.Sort();
+            }
+      }
 }
 
